047a85839e59687bcab16bc6a1f2e89d4d8da08d
Cezar Garrido Britez <cezar.britez@americanas.io>
2025-02-20 23:01:48 -0300

    chore: add i686

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, PrintOperation};
+use fltk::{
+    app, button::Button, draw, enums, prelude::*, printer, window::Window
+};
 
 fn main() {
-    let application = Application::builder()
-        .application_id("com.example.FirstGtkApp")
-        .build();
-
-    application.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("First GTK Program")
-            .default_width(350)
-            .default_height(70)
-            .build();
-
-        let button = Button::with_label("Print");
-        button.connect_clicked(move |_| {
-            let print_operation = PrintOperation::new();
-            
-            
-            print_operation.set_allow_async(true);
-            
-            print_operation.run(gtk::PrintOperationAction::PrintDialog, None::<&ApplicationWindow>);
-        });
-        
-        window.add(&button);
-        window.show_all();
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "FLTK Printer Example");
+    let mut but = Button::new(150, 125, 100, 40, "Print");
+    
+    but.set_callback(|widget| {
+        let mut printer = printer::Printer::default();
+        if printer.begin_job(1).is_ok() {
+            printer.begin_page().ok();
+            let (width, height) = printer.printable_rect();
+            draw::set_draw_color(enums::Color::Black);
+            draw::set_line_style(draw::LineStyle::Solid, 2);
+            draw::draw_rect(0, 0, width, height);
+            draw::set_font(enums::Font::Courier, 12);
+            printer.set_origin(width / 2, height / 2);
+            printer.print_widget(widget, -widget.width() / 2, -widget.height() / 2);
+            printer.end_page().ok();
+            printer.end_job();
+        }
     });
-
-    application.run();
+    
+    wind.end();
+    wind.show();
+    app.run().unwrap();
 }
