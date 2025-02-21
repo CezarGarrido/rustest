e7823420875ce33a3fb5be7ccbe57aa682bbed1e
Cezar Garrido Britez <cezar.britez@americanas.io>
2025-02-21 10:19:04 -0300

    chore: add gtk

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,107 @@
-use fltk::{
-    app, button::Button, draw, enums, prelude::*, printer, window::Window
+use gtk::prelude::*;
+use gtk::{gio, glib};
+use gtk::{
+    AboutDialog, AccelFlags, AccelGroup, ApplicationWindow, CheckMenuItem, IconSize, Image, Label,
+    Menu, MenuBar, MenuItem, WindowPosition,
 };
 
 fn main() {
-    println!("Iniciando...");
-    let app = app::App::default();
-    let mut wind = Window::new(100, 100, 400, 300, "FLTK Printer Example");
-    let mut but = Button::new(150, 125, 100, 40, "Print");
-    
-    but.set_callback(|widget| {
-        let mut printer = printer::Printer::default();
-        if printer.begin_job(1).is_ok() {
-            printer.begin_page().ok();
-            let (width, height) = printer.printable_rect();
-            draw::set_draw_color(enums::Color::Black);
-            draw::set_line_style(draw::LineStyle::Solid, 2);
-            draw::draw_rect(0, 0, width, height);
-            draw::set_font(enums::Font::Courier, 12);
-            printer.set_origin(width / 2, height / 2);
-            printer.print_widget(widget, -widget.width() / 2, -widget.height() / 2);
-            printer.end_page().ok();
-            printer.end_job();
-        }
+
+    let application = gtk::Application::new(
+        Some("com.github.gtk-rs.examples.menu_bar"),
+        Default::default(),
+    );
+
+    application.connect_activate(build_ui);
+
+    application.run();
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = ApplicationWindow::new(application);
+
+    window.set_title("MenuBar example");
+    window.set_position(WindowPosition::Center);
+    window.set_size_request(400, 400);
+
+    let v_box = gtk::Box::new(gtk::Orientation::Vertical, 10);
+
+    let menu = Menu::new();
+    let accel_group = AccelGroup::new();
+    window.add_accel_group(&accel_group);
+    let menu_bar = MenuBar::new();
+    let file = MenuItem::with_label("File");
+    let about = MenuItem::with_label("About");
+    let quit = MenuItem::with_label("Quit");
+    let file_item = MenuItem::new();
+    let file_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+    let file_image = Image::from_resource("/org/gtk-rs/examples/file.png");
+    let file_label = Label::new(Some("File"));
+    let folder_item = MenuItem::new();
+    let folder_box = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+    let folder_image = Image::from_icon_name(Some("folder-music-symbolic"), IconSize::Menu);
+    let folder_label = Label::new(Some("Folder"));
+    let check_item = CheckMenuItem::with_label("Click me!");
+
+    file_box.pack_start(&file_image, false, false, 0);
+    file_box.pack_start(&file_label, true, true, 0);
+    file_item.add(&file_box);
+    folder_box.pack_start(&folder_image, false, false, 0);
+    folder_box.pack_start(&folder_label, true, true, 0);
+    folder_item.add(&folder_box);
+    menu.append(&file_item);
+    menu.append(&folder_item);
+    menu.append(&check_item);
+    menu.append(&about);
+    menu.append(&quit);
+    file.set_submenu(Some(&menu));
+    menu_bar.append(&file);
+
+    let other_menu = Menu::new();
+    let sub_other_menu = Menu::new();
+    let other = MenuItem::with_label("Another");
+    let sub_other = MenuItem::with_label("Sub another");
+    let sub_other2 = MenuItem::with_label("Sub another 2");
+    let sub_sub_other2 = MenuItem::with_label("Sub sub another 2");
+    let sub_sub_other2_2 = MenuItem::with_label("Sub sub another2 2");
+
+    sub_other_menu.append(&sub_sub_other2);
+    sub_other_menu.append(&sub_sub_other2_2);
+    sub_other2.set_submenu(Some(&sub_other_menu));
+    other_menu.append(&sub_other);
+    other_menu.append(&sub_other2);
+    other.set_submenu(Some(&other_menu));
+    menu_bar.append(&other);
+
+
+    // `Primary` is `Ctrl` on Windows and Linux, and `command` on macOS
+    // It isn't available directly through `gdk::ModifierType`, since it has
+    // different values on different platforms.
+    let (key, modifier) = gtk::accelerator_parse("<Primary>Q");
+    quit.add_accelerator("activate", &accel_group, key, modifier, AccelFlags::VISIBLE);
+
+    let label = Label::new(Some("MenuBar example"));
+
+    v_box.pack_start(&menu_bar, false, false, 0);
+    v_box.pack_start(&label, true, true, 0);
+    window.add(&v_box);
+    window.show_all();
+
+    about.connect_activate(move |_| {
+        let p = AboutDialog::new();
+        p.set_authors(&["gtk-rs developers"]);
+        p.set_website_label(Some("gtk-rs"));
+        p.set_website(Some("http://gtk-rs.org"));
+        p.set_authors(&["gtk-rs developers"]);
+        p.set_title("About!");
+        p.set_transient_for(Some(&window));
+        p.show_all();
     });
-    
-    wind.end();
-    wind.show();
-    app.run().unwrap();
+    check_item.connect_toggled(|w| {
+        w.set_label(if w.is_active() {
+            "Checked"
+        } else {
+            "Unchecked"
+        });
+    });
 }
